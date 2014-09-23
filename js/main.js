3dbef45044434f3dfa312a2b75060427b8fa0cc9
Michael Hogg <michael.hogg.uk@gmail.com>
2014-09-23 19:35:36 +0100

    Move Controller code into main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 /* Initialize everything. */
-var gol = null, controller = null;
+
+var gol = null;
+
 $(document).ready(function() {
+
     var $canvas = $('#life');
     gol = new GOL($canvas[0]).draw().start();
-    controller = new Controller(gol);
+
+    $(document).on('keyup', function(event) {
+        switch (event.which) {
+            case 82: /* r */
+                gol.setRandom();
+                gol.draw();
+                break;
+            case 83: /* s */
+                gol.toggle();
+                break;
+        }
+    });
+
 });
