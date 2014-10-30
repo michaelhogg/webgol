68cfea2fad670debef56569c31ce76be9f2094af
Michael Hogg <michael.hogg.uk@gmail.com>
2014-10-30 18:01:28 +0000

    Create webgol_core task and add comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
         /*--- Data for templates ---*/
 
+        // Files to lint for terminating newlines and trailing whitespace
         lintspaces_src: [
             '.gitignore',
             '.jshintrc',
@@ -18,18 +19,20 @@ module.exports = function(grunt) {
         ],
 
 
-        /*--- Tasks ---*/
+        /*--- Core tasks ---*/
 
+        // Lint for terminating newlines and trailing whitespace
         lintspaces: {
             webgol: {
                 options: {
-                    newline:        true,
-                    trailingspaces: true
+                    newline:        true,  // Check for one newline at end of every file
+                    trailingspaces: true   // Check for any trailing whitespace
                 },
                 src: ['<%= lintspaces_src %>']
             }
         },
 
+        // Lint JavaScript files
         jshint: {
             webgol: {
                 options: {
@@ -39,6 +42,19 @@ module.exports = function(grunt) {
             }
         },
 
+        // Compile SCSS to CSS
+        compass: {
+            webgol: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+
+
+        /*--- Dev tasks ---*/
+
+        // Concatenate JavaScript files without minifying
         concat: {
             webgol: {
                 files: {
@@ -48,19 +64,15 @@ module.exports = function(grunt) {
             }
         },
 
+
+        /*--- Prod tasks ---*/
+
+        // Concatenate and minify JavaScript files
         uglify: {
             webgol: {
                 files: {
                     'jsmin/gol.js':  ['js/gol/**/*.js'],
                     'jsmin/main.js': ['js/main.js']
-                }
-            }
-        },
-
-        compass: {
-            webgol: {
-                options: {
-                    config: 'config.rb'
                 }
             }
         }
@@ -75,8 +87,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
 
-    grunt.registerTask('webgol_dev',  ['lintspaces', 'jshint', 'concat', 'compass']);
-    grunt.registerTask('webgol_prod', ['lintspaces', 'jshint', 'uglify', 'compass']);
+    grunt.registerTask('webgol_core', ['lintspaces', 'jshint', 'compass']);
+
+    grunt.registerTask('webgol_dev',  ['webgol_core', 'concat']);
+    grunt.registerTask('webgol_prod', ['webgol_core', 'uglify']);
 
     grunt.registerTask('default', ['webgol_dev']);
 
